39a81c0eb015a58b24e3821576481b980fb53dcd
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-03-05 10:18:40 -0700

    update: add LexicalErrors structure

--- include/Lexer.hpp
+++ include/Lexer.hpp
@@ -4,19 +4,31 @@
 #include "Data/TransitionMatrix.hpp"
 #include "List.hpp"
 #include <string>
+#include <vector>
+
+struct LexicalErrors {
+  int line;
+  int column;
+  std::string message;
+  std::string lexeme;
+};
 
 class Lexer {
 private:
   int state;
   int category;
+  int line;
+  int column;
   std::string buffer;
   List TokenList;
+  std::vector<LexicalErrors> errors;
 
 public:
   Lexer();
   lexer::Symbol classifyChar(char c);
   void createTokenList(char c);
   void printTokenList();
+  void printErrors();
 };
 
 #endif
